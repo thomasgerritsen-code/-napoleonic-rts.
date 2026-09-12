4e051ddc67a87ac207f356d583e0cad7d4636de0
thomasgerritsen-code <thomasgerritsen@outlook.com>
2026-09-12 07:04:16 +0200

    Release v1.3.1 3D performance hotfix

--- src/foundation/version.js
+++ src/foundation/version.js
@@ -1,6 +1,6 @@
 'use strict';
 (function initRtsVersion(root) {
-  const VERSION = '1.3.0';
+  const VERSION = '1.3.1';
   const app = document.getElementById('app');
   if (app) app.style.visibility = 'hidden';
 
