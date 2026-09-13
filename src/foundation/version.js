a36beb7de8352bbff7cec53f1eaed43da3268c58
thomasgerritsen-code <thomasgerritsen@outlook.com>
2026-09-13 03:01:32 +0200

    Bump runtime version to v1.3.7

--- src/foundation/version.js
+++ src/foundation/version.js
@@ -1,6 +1,6 @@
 'use strict';
 (function initRtsVersion(root) {
-  const VERSION = '1.3.4';
+  const VERSION = '1.3.7';
   const app = document.getElementById('app');
   if (app) app.style.visibility = 'hidden';
 
